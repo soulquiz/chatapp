bcffd2284fcd8574ff46e288e39b4891291b2638
Chatchai Honarongsiri <soulquiza@gmail.com>
2018-05-12 16:32:08 +0700

    connect to client2 socketio

--- server.js
+++ server.js
@@ -9,13 +9,17 @@ const server = app.listen(APP_PORT, () => {
 })
 
 const io = require('socket.io').listen(server)
-
+const desIO = require('socket.io-client')('http://192.168.1.108:8080')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
+  desIO.on('connect', function () {
+    console.log('client2 connected')
+  })
+  desIO.emit('chat', 'client1 sent')
   res.render('index')
 })
 
