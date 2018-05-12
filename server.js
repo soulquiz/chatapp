cd647cf6a57a649aae86a2a436fad70929a75101
Chatchai Honarongsiri <soulquiza@gmail.com>
2018-05-12 15:49:55 +0700

    add socket io to the project

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-const APP_PORT = 8080
+const APP_PORT = process.env.port || 8080
 
 const server = app.listen(APP_PORT, () => {
   console.log(`App running on port ${APP_PORT}`)
@@ -10,20 +10,15 @@ const server = app.listen(APP_PORT, () => {
 
 const io = require('socket.io').listen(server)
 
-// // ตั้งค่า เพื่อให้ express ทำการ render view ที่โฟลเดอร์ views
-// // และใช้ template engine เป็น pug
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
   res.render('index')
 })
 
 io.on('connection', function (socket) {
-  socket.on('chatter', (message) => {
-    console.log('message : ', message)
-    io.emit('chatter', message)
-  })
+  console.log('hello')
 })
